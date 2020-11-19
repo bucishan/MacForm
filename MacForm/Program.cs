dc90ea7fbad5afcf9405e443cf79803073e7a285
smartizans <i@shanli.ren>
2020-11-19 11:10:00 +0800

    添加带背景图像测试窗口

--- MacForm/Program.cs
+++ MacForm/Program.cs
@@ -15,7 +15,7 @@ namespace MacForm
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new BaseForm());
+            Application.Run(new Test());
         }
     }
 }
